33eeef72b9d9e54f06be599d538dd1c093af930f
SherriWeb <abdelhamidshirin@gmail.com>
2023-06-15 14:31:08 -0700

    add

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
 /**
-* Description: A program with C language.
-* function main - Entry point of the program.
-* Return value : always 0 (success).
-* main - A program that print a line using the printf.
+ *  main - A program that prints using printf.
+ *  Return: Always 0 (Success)
 */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
-
