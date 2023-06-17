a2dd96e0ad7a019bf7424c39fe803f299b4de591
SherriWeb <abdelhamidshirin@gmail.com>
2023-06-16 18:15:57 -0700

    add

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *  main - A program that print alphabets in reverse.
+ *  Return: Always 0 (Success)
+*/
+int main(void)
+{
+int n;
+
+for (n = 48; n <= 57; n++)
+{
+putchar(n);
+if (n == 57)
+{
+continue;
+}
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}
