ad9689aa10404c288a6f281e24f13eb32ac278a1
SherriWeb <abdelhamidshirin@gmail.com>
2023-06-16 17:05:53 -0700

    add

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *  main - A program that prints alphabets.
+ *  Return: Always 0 (Success)
+*/
+int main(void)
+{
+int n = 97;
+
+while (n <= 122)
+{
+putchar(n);
+n++;
+}
+putchar('\n');
+return (0);
+}
