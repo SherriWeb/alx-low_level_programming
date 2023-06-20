cbdc4501ed3becec2f65d913cd02a5423f831674
SherriWeb <abdelhamidshirin@gmail.com>
2023-06-20 07:48:37 -0700

    dd

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *  main - Entry point.
+ *  Description : return integer.
+ *  Return: Always 0 (Success).
+*/
+int _abs(int i)
+if ( i >= 0)
+{
+return (i);
+}
+else 
+{
+return (i * -1);
+}
