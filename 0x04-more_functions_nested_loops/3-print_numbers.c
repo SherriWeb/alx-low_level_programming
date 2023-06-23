f2030246919b726d47f8a889e551c2860bb102e8
SherriWeb <abdelhamidshirin@gmail.com>
2023-06-22 17:48:58 -0700

    agg

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -5,10 +5,10 @@
 */
 void print_numbers(void)
 {
-int num = 0;
-do {
-_putchar(num + 48);
-num++;
-} while (num >= 0 && num <= 9);
-_putchar('\n')
+int x;
+for (x = 0; x <= 9; x++)
+{
+_putchar(x + '0');
+}
+_putchar('\n');
 }
