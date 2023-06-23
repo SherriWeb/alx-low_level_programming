8921c027311da5389831d8b00a45255a41ef4c81
SherriWeb <abdelhamidshirin@gmail.com>
2023-06-22 17:19:12 -0700

    add

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,17 +6,12 @@
 */
 void print_most_numbers(void)
 {
-int x = 0;
-for (; x <= 0; x++)
+int num;
+for (num = 0; num <= 9; x++)
 {
-if (x == 2 || x == 4)
-{
+if (num == 2 || num == 4)
 continue;
-}
-else
-{
-_putchar( x + '0');
-}
+_putchar(num + 48);
 }
 _putchar('\n');
 }
