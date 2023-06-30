2b8387c77adfa8aa6cbe61dd75185aae5b356166
SherriWeb <abdelhamidshirin@gmail.com>
2023-06-29 17:09:29 -0700

    add

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,16 +7,13 @@
 */
 void print_number(int n)
 {
-unsigned int n1;
-n1 = n;
+unsigned int num = n;
 if (n < 0)
 {
 putchar('-');
-n1 = -n;
+num = -num;
 }
-if (n1 / 10 != 0)
-{
-print_number(n1 / 10);
-}
-putchar((n1 % 10) + '0');
+if ((num / 10) > 0)
+print_number(num / 10);
+putchar((num % 10) + '0');
 }
