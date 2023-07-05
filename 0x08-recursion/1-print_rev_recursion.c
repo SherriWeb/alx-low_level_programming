a9e000173cc34859f6be284b758ce98e6001111e
SherriWeb <abdelhamidshirin@gmail.com>
2023-07-05 16:20:19 -0700

    add

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+/**
+ *  puts_recursion - Entry point.
+ *  Description:Writing a function that prints a string in reverse.
+ *  @s: a string to reverse.
+ *  Return: nothing.
+*/
+void _print_rev_recursion(char *s)
+{
+if (*s == '\0')
+{
+return;
+}
+s++;
+_print_rev_recursion(s);
+s--;
+_putchar(*s);
+}
